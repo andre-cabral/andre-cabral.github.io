9382604902f0a47c68754f907baa814b490632fe
Andre Cabral <al.cabral.89@gmail.com>
2018-04-13 11:18:32 -0300

    ddmm update

--- ddmm/ddmm.js
+++ ddmm/ddmm.js
@@ -3,8 +3,38 @@ var ddmmArray = new Array();
 $(document).ready(function(){
 	ddmmArray = $('#list option');
 	listChangeEvent();
+	getHashSpells()
 	addButtonClickEvent();
 });
+
+function getHashSpells(){
+	var spellHashes = document.location.hash.split('|');
+	for(var i=0; i<spellHashes.length; i++){
+		if(i == 0){
+			showSpell(spellHashes[i].replace('#', ''));
+		}else{
+			showSpell(spellHashes[i]);
+		}
+		
+		addSpellHash();
+	}
+}
+
+function addSpellHash(){
+	var toAdd = $('#selected_spell').html();
+	if(toAdd != ''){
+		$(".to_print").append('<div class="spell">'+toAdd+'</div>');
+	    
+	    var childs = $(".to_print").children();
+	    for(var i=0; i < childs.length; i++){
+	        if((i+1)%3 ==0){
+	            //$(childs[i]).addClass("spell_last");
+	            //$(childs[i]).append('<br />');
+	            $(".to_print")
+	        }
+	    }
+    }
+}
 
 function listChangeEvent(){
 	$("#list").change(listChange);
@@ -25,7 +55,7 @@ function showSpell(selected){
 		});
 		if(selectedSpellObject){
 			$("#selected").html("");
-			$("#selected").append('<img src="'+selectedSpellObject.value+'" />');
+			$("#selected").append('<img class="'+selected+'" src="'+selectedSpellObject.value+'" />');
 		}
 	}
 }
@@ -35,6 +65,12 @@ function addButtonClickEvent(){
 		var toAdd = $('#selected').html();
 		$(".to_print").append('<div class="ddmm">'+toAdd+'</div>');
         
+		if(document.location.hash == ''){
+			document.location.hash += $('#selected').children('img').class();
+        }else{
+			document.location.hash += '|'+$('#selected').children('img').class();
+        }
+
         var childs = $(".to_print").children();
         for(var i=0; i < childs.length; i++){
             if((i+1)%3 ==0){
