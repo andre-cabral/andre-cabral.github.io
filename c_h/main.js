b3acee660889377e8ab2a9cdd604772305ee2198
Andre Cabral <al.cabral.89@gmail.com>
2019-02-26 00:51:57 -0300

    c_h
    c_h

--- c_h/main.js
+++ c_h/main.js
@@ -1,21 +1,26 @@
 $(function() {
+  /********VARIABLES*/
+  var bodyParts = ['CABEÇA', 'TRONCO', 'BRAÇOS', 'MÃOS', 'PERNAS', 'PÉS'];
+  var bodyPartIndex = 0;
   var alphabeth = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
-  var word = ['C', 'A', 'B', 'E', 'Ç', 'A'];
+  var word = getActualBodyPart().split('');
   var lastPlayedSound = '';
-
   var audio = document.querySelector('audio');
-  var percentages = document.querySelectorAll('.loading-audio');
 
+
+  //var percentages = document.querySelectorAll('.loading-audio');
+
+  /*
   function loop() {
     var buffered = audio.buffered;
     var loaded;
-/*    var played;*/
+    //var played;
 
     if (buffered.length) {
       loaded = 100 * buffered.end(0) / audio.duration;
-      /*played = 100 * audio.currentTime / audio.duration;*/
+      //played = 100 * audio.currentTime / audio.duration;
       percentages[0].innerHTML = loaded.toFixed(2);
-/*      percentages[1].innerHTML = played.toFixed(2);*/
+      //percentages[1].innerHTML = played.toFixed(2);
     }
 
     if( !buffered.length || loaded.toFixed(2) < 100) {
@@ -24,17 +29,22 @@ $(function() {
   }
 
   loop();
+*/
 
+
+
+  /********GAME MECHANICS********/
   for (var i=0; i<alphabeth.length; i++) {
     $("#container-letters")
       .append(
         '<div id="draggable-' + i + '" class="draggable-letter">'+
-          '<div class="letter-bg">' + alphabeth[i] + '</div>'+
+          '<div id="letter-bg-' + i + '" class="letter-bg">' + alphabeth[i] + '</div>'+
         '</div>');
     $( "#draggable-" + i ).attr('data-letter', alphabeth[i]);
+    $( "#letter-bg-" + i ).attr('data-letter', alphabeth[i]);
     
-    $( "#draggable-" + i ).mousedown(function() {
-      playSound("audio-"+alphabeth[i]);
+    $( "#draggable-" + i ).mousedown(function(event) {
+      playSound("audio-"+$(event.target).attr('data-letter'));
       $(this).addClass("dragging");
     });
 
@@ -57,24 +67,59 @@ $(function() {
       }
     });
   }
-  for (var i=0; i<word.length; i++) {
-    $("#container-drops").append('<div id="droppable-' + i + '" class="droppable-letter"> </div>')
-    $( "#droppable-" + i ).attr('data-letter', word[i]);
 
-    if(!isAccentedCharacter(word[i])) {
-      $( "#droppable-" + i ).droppable({
-        tolerance: "pointer",
-        drop: function( event, ui ) {
-          isCorrect(event, ui);
-        }
-      });
-    } else {
-      $( "#droppable-" + i )
-      .addClass( "correct" )
-      .text( word[i] );
-    }
+  /***********STICKERS***************/
+  var audio = document.querySelector('audio');
+  for (var i=0; i<bodyParts.length; i++) {
+    $("#container-sticker-images")
+      .append(
+        '<div id="sticker-' + i + '" class="sticker sticker-'+ getBodyPartCode(i) +'"></div>'
+      );
+    $( "#sticker-" + i ).attr('data-body-part', getBodyPartCode(i));
+    $( "#sticker-" + i ).mousedown(function(event) {
+      playSound("audio-"+$(event.target).attr('data-body-part'));
+      $(this).addClass("dragging");
+    });
+    $( "#sticker-" + i ).mouseup(function() {
+      $(this).removeClass("dragging");
+    })
+    $( "#sticker-" + i ).draggable({
+      scroll: false,
+      revert: "invalid",
+      revertDuration: 0,
+      start: function() {
+        /**/
+      },
+      drag: function() {
+        /**/
+      },
+      stop: function(event, ui) {
+        $(this).removeClass("dragging");
+      }
+    });
+
+
+    $("#container-sticker-body").append(
+      '<div id="droppable-sticker-' + i + '" class="droppable-sticker droppable-sticker-'+ getBodyPartCode(i) +'"></div>'
+    );
+    $( "#droppable-sticker-" + i ).attr('data-body-part', getBodyPartCode(i));
+    $( "#droppable-sticker-" + i ).droppable({
+      tolerance: "pointer",
+      accept: ".sticker",
+      drop: function( event, ui ) {
+        isCorrectSticker(event, ui);
+      }
+    });
+
+    $('#container-parts-list').append(
+      '<div id="part-name-' + i + '" class="part-name part-name-'+ getBodyPartCode(i) +'">'+ bodyParts[i] +'</div>'
+    );
   }
 
+  /**********INIT****************/
+  setBodyPart();
+
+  /**********FUNCTIONS***********/
   function isCorrect(event, ui) {
     var _droppable = $(event.target);
     var _draggable = $(ui.draggable[0]);
@@ -89,6 +134,8 @@ $(function() {
       playSound("correct");
 
       fadeOutLetter(_draggable);
+
+      checkAllCorrect();
     } else {
       if (word.indexOf(_draggable.attr('data-letter')) > -1) {
         $(_draggable).addClass('half-correct');
@@ -104,8 +151,36 @@ $(function() {
     }
   }
 
-  function isAccentedCharacter (letter) {
-    return letter.match(/[áàâãéèêíïóôõöúçñÁÀÂÃÉÈÍÏÓÔÕÖÚÇÑ]/);
+  function checkAllCorrect() {
+    if ($('.droppable-letter.correct').length >= getActualBodyPart().length) {
+      $('.container-mecanica-forca').removeClass('next');
+      $('.container-mecanica-forca').addClass('all-correct');
+      $("#droppable-sticker-"+bodyPartIndex).css('display', 'block');
+      $("#sticker-"+bodyPartIndex).css('display', 'block');
+      $("#container-sticker-name").text(getActualBodyPart());
+      $("#part-name-"+bodyPartIndex).css('display', 'block');
+
+      setTimeout(function(){
+        $('.container-mecanica-forca').css('display', 'none');
+      }, 2000);
+    }
+  }
+
+  function isCorrectSticker(event, ui) {
+    var _droppable = $(event.target);
+    var _draggable = $(ui.draggable[0]);
+
+    $(_draggable).css('display', 'none');
+    $( _droppable )
+    .droppable( "option", "disabled", true )
+    .addClass( "correct" )
+    //.text( _droppable.attr('data-letter') );
+    playSound("correct");
+    $('.part-name-'+getActualBodyPartCode()).css('opacity', 1);
+
+    nextBodyPart();
+
+    //fadeOutLetter(_draggable);
   }
 
   function fadeOutLetter(letter) {
@@ -135,5 +210,71 @@ $(function() {
 
     $("#" + soundId)[0].play();
     lastPlayedSound = soundId;
+  }
+
+  function getActualBodyPart() {
+    return bodyParts[bodyPartIndex];
+  }
+  
+  function getActualBodyPartCode() {
+    return removeAccents(getActualBodyPart()).toLowerCase();
+  }
+
+  function getBodyPartCode(i) {
+    return removeAccents(bodyParts[i]).toLowerCase();
+  }
+
+  function setBodyPart() {
+    $('.container-mecanica-forca').removeClass('all-correct');
+    $('.container-mecanica-forca').css('display', 'block');
+    word = bodyParts[bodyPartIndex].split('');
+
+    $("#container-drops").empty();
+    for (var i=0; i<word.length; i++) {
+      $("#container-drops").append('<div id="droppable-' + i + '" class="droppable-letter"> </div>')
+      $( "#droppable-" + i ).attr('data-letter', word[i]);
+  
+      if(!isAccentedCharacter(word[i])) {
+        $( "#droppable-" + i ).droppable({
+          tolerance: "pointer",
+          accept: ".draggable-letter",
+          drop: function( event, ui ) {
+            isCorrect(event, ui);
+          }
+        });
+      } else {
+        $( "#droppable-" + i )
+        .addClass( "correct" )
+        .text( word[i] );
+      }
+    }
+  }
+  function nextBodyPart() {
+    bodyPartIndex ++;
+    if (bodyPartIndex < bodyParts.length) {
+      $('.container-mecanica-forca').addClass('next');
+      setBodyPart();
+    } else {
+      /* END GAME HERE */
+    }
+  }
+
+  function isAccentedCharacter (letter) {
+    return letter.match(/[áàâãéèêíïóôõöúçñÁÀÂÃÉÈÍÏÓÔÕÖÚÇÑ]/);
+  }  
+  function removeAccents(strAccents) {
+    var strAccents = strAccents.split('');
+    var strAccentsOut = new Array();
+    var strAccentsLen = strAccents.length;
+    var accents = 'ÀÁÂÃÄÅàáâãäåÒÓÔÕÕÖØòóôõöøÈÉÊËèéêëðÇçÐÌÍÎÏìíîïÙÚÛÜùúûüÑñŠšŸÿýŽž';
+    var accentsOut = "AAAAAAaaaaaaOOOOOOOooooooEEEEeeeeeCcDIIIIiiiiUUUUuuuuNnSsYyyZz";
+    for (var y = 0; y < strAccentsLen; y++) {
+      if (accents.indexOf(strAccents[y]) != -1) {
+        strAccentsOut[y] = accentsOut.substr(accents.indexOf(strAccents[y]), 1);
+      } else
+        strAccentsOut[y] = strAccents[y];
+    }
+    strAccentsOut = strAccentsOut.join('');
+    return strAccentsOut;
   }
 });
