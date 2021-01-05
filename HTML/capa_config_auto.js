8fc596378e98e74e25db6b59b63751311c406568
Andre Cabral <cabral.dev@gmail.com>
2021-01-04 21:03:41 -0300

    html

---
+++ HTML/capa_config_auto.js
@@ -0,0 +1,20 @@
+var CAPA_CONFIG_AUTO = {
+    "capa_version": "1.4.4",
+    "logo": "ftd",
+    "idioma": "pt-BR",
+    "expediente": [
+        [
+            "FTD - Sistema de Ensino",
+            "Diretor de Conte\u00fado e Neg\u00f3cios\r\nRicardo Tavares de Oliveira\r\n\r\nGerente Editorial\r\nJ\u00falio C\u00e9sar D. da Silva Ibrahim\r\n\r\nEditora\r\nLuzia Estev\u00e3o Garcia\r\n\r\nEditores Assistentes\r\nAlan Mazoni Alves, Amanda Carvalho de Oliveira, Fernanda Batista dos Santos, Fernanda Zacharias Buys, Gisele Meleiro Rodrigues, Isabela Gorgatti Cruz, Luciana Moura, Simone Saty Sempe, Thais Sanches Domingues, Vivian Gusm\u00e3o, Victor Seiji Fujiwara"
+        ],
+        [
+            "Produ\u00e7\u00e3o Digital",
+            "Coordenador de produ\u00e7\u00e3o digital\r\nRenan Sucupira\r\n\r\nAnalista de solu\u00e7\u00e3o digital\r\nJanaina Chervezan\r\n\r\nAssistentes de solu\u00e7\u00e3o digital\r\nJun Sugiyama, L\u00edvia Stevaux\r\n\r\nPublica\u00e7\u00e3o digital\r\nSid Viana\r\n\r\nControle de qualidade digital\r\nRenata Savero\r\n\r\nCoordenadora de prepara\u00e7\u00e3o e revis\u00e3o\r\nLilian Semenichin\r\n \r\nSupervisoras de prepara\u00e7\u00e3o e revis\u00e3o\r\nAdriana Soares de Souza, Beatriz Carneiro, Viviam Moreira\r\n \r\nPrepara\u00e7\u00e3o e revis\u00e3o de textos\r\nEquipe FTD\r\n\r\nSupervisora de Iconografia\r\nElaine Bueno\r\n\r\nIcon\u00f3grafas\r\nEquipe FTD"
+        ],
+        [
+            "TowerLab Studios",
+            "Designer\r\nCelso Fujimoto\r\n\r\nProdutor\r\nRaphael Martinez\r\n\r\nDesenvolvedor\r\nFrancisco Escobar\r\n\r\nIlustra\u00e7\u00e3o\r\nDaiandreson DRADRA Victor"
+        ]
+    ],
+    "creditos": false
+};
