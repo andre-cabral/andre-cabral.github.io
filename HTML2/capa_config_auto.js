efce1562059c05f825f4987ccf86bf9d6f71f79c
Andre Cabral <cabral.dev@gmail.com>
2021-01-26 22:07:27 -0300

    html2

---
+++ HTML2/capa_config_auto.js
@@ -0,0 +1,20 @@
+var CAPA_CONFIG_AUTO = {
+    "capa_version": "2.0.0",
+    "logo": "ftd",
+    "idioma": "pt-BR",
+    "expediente": [
+        [
+            "Conte\u00fado e Neg\u00f3cios",
+            "Diretor-geral\r\nRicardo Tavares de Oliveira\r\n\r\nDiretora educacional\r\nCeciliany Alves\r\n\r\nDiretor adjunto\r\nCayube Galas\r\n\r\nGerente de conte\u00fado\r\nJ\u00falio Ibrahim\r\n\r\nEditora\r\nVivian Kaori Ehara\r\n\r\nEditora assistente\r\nCarolina Evangelista\r\n\r\nSupervisora de prepara\u00e7\u00e3o e revis\u00e3o\r\nAdriana Soares de Souza\r\n\r\nPrepara\u00e7\u00e3o e revis\u00e3o\r\nEquipe FTD\r\n\r\nColaboradores\r\nBoris Fatigati, Eduardo Quintanilha Oficina Editorial, Erika Alonso, Magueda Lopes Souza, Michele Tessaroto, Rita de C\u00e1ssia Cruz Silva"
+        ],
+        [
+            "Produ\u00e7\u00e3o Digital",
+            "Coordenador de Produ\u00e7\u00e3o Digital\r\nCarlos Bueno\r\n\r\nProdu\u00e7\u00e3o Digital\r\nJanaina Chervezan\r\n\r\nAssistentes de Produ\u00e7\u00e3o Digital\r\nJun Sugiyama, L\u00edvia Stevaux\r\n\r\nPublica\u00e7\u00e3o Digital\r\nSid Viana\r\n\r\nAssistentes de Publica\u00e7\u00e3o Digital\r\nJuliana Santos, Jeniffer Guimar\u00e3es\r\n\r\nControle de Qualidade Digital\r\nThays Oyakawa\r\n\r\nAssistentes de Qualidade Digital\r\nAndressa Comar Seixas, Heitor Ferreira Mota, Helena Ariano, Renata Slovac Savero, Thais Lima Neves Vieira\r\n\r\nSupervisor de Desenvolvimento\r\nAndr\u00e9 Lu\u00eds Machado\r\n\r\nDesenvolvedores Web\r\nBruno Caramelo Souza, Rafael Cardoso da Silva"
+        ],
+        [
+            "TowerLab Studios",
+            "Designer\r\nCelso Fujimoto\r\n\r\nProdutor\r\nRaphael Martinez\r\n\r\nDesenvolvedor\r\nAndr\u00e9 Cabral\r\n\r\nIlustra\u00e7\u00e3o\r\nFelipe Lopes, Aleff Rodrigues"
+        ]
+    ],
+    "creditos": false
+};
